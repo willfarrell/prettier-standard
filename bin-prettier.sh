f46978a5618cb685c1548717270db6daf0c56fd0
will Farrell <will.farrell@gmail.com>
2018-04-23 16:21:50 -0600

    perf: add in options to match standard

--- bin-prettier.sh
+++ bin-prettier.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "$0" )" && pwd )/$(dirname "$(readlink "$0")")/node_modules/.bin"
-$DIR/prettier --write --loglevel=warn $1
+$DIR/prettier --write --loglevel=warn --no-semi --single-quote "$1"
