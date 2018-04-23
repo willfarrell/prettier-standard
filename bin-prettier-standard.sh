a8c63b493f5e18f614ce90da64caf5cf17264359
will Farrell <will.farrell@gmail.com>
2018-04-22 19:59:31 -0600

    fix: missing paths

--- bin-prettier-standard.sh
+++ bin-prettier-standard.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./bin-prettier.sh $1
-./bin-standard.sh $1
+./node_modules/.bin/glob-cli $1 | xargs ./node_modules/.bin/prettier --write --loglevel=warn
+./node_modules/.bin/glob-cli $1 | grep -e '\.js$' | ./node_modules/.bin/standard --fix
